8dea6210d483d59e12825ada9beded8974392808
Matzuc2 <mathieu.zucalli2@holbertonschool.com>
2025-01-15 12:53:14 +0100

    correcting all pycodestyle

--- debugging/print_arguments.py
+++ debugging/print_arguments.py
@@ -1,8 +1,5 @@
+#!/usr/bin/python3
 import sys
 
-if len(sys.argv) <= 1:
-    print("No arguments provided.")
-else:
-    # Print only the arguments, excluding the script name (sys.argv[0])
-    for arg in sys.argv[1:]:
-        print(arg)
+for i in range(1, len(sys.argv)):
+    print(sys.argv[i])
