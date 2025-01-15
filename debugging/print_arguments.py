d418fc74df7bf2d8b7ef6e7fff7fab0a44fe6324
Matzuc2 <mathieu.zucalli2@holbertonschool.com>
2025-01-15 12:51:00 +0100

    adding new code to print the arguments but not the script name

--- debugging/print_arguments.py
+++ debugging/print_arguments.py
@@ -3,5 +3,6 @@ import sys
 if len(sys.argv) <= 1:
     print("No arguments provided.")
 else:
+    # Print only the arguments, excluding the script name (sys.argv[0])
     for arg in sys.argv[1:]:
         print(arg)
